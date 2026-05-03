be0d35381f87374a681617de2dad63135ed3cfdb
Fastman <gabayt0_0@icloud.com>
2026-05-03 17:36:51 +0900

    chore: home-manager init

---
+++ home.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "hayato";
+  home.homeDirectory = "/Users/hayato";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/hayato/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
