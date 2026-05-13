15ff02f831415b59b678695526e0d43913bb2d17
Fastman <gabayt0_0@icloud.com>
2026-05-14 00:17:45 +0900

    chore: temporarily override Git package to version 2.54.0 with upstream tarball for new features

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,8 +1,27 @@
 { config, pkgs, ... }:
 
+let
+  # nixpkgs が git 2.54 を取り込むまでの暫定 override。
+  # 2.54 で導入された機能を使うため、upstream tarball を直接差し替える。
+  # nixpkgs 側が 2.54 以上になったらこのブロックごと削除する。
+  gitWithUpstream = pkgs.git.overrideAttrs (old: rec {
+    version = "2.54.0";
+    src = pkgs.fetchurl {
+      url = "https://www.kernel.org/pub/software/scm/git/git-${version}.tar.xz";
+      sha256 = "sha256-9okWI2TBDeee+Jqo2/SHMesFfjTtu9IKylEM4BVGgaM=";
+    };
+    # 2.54 で upstream にマージ済み。重複適用を避けるため除外する。
+    patches = builtins.filter
+      (p: !(pkgs.lib.hasSuffix
+        "osxkeychain-define-build-targets-in-toplevel-Makefile.patch"
+        (toString p)))
+      old.patches;
+  });
+in
 {
   programs.git = {
     enable = true;
+    package = gitWithUpstream;
 
     settings = {
       user = {
